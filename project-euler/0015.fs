4ea427275ff3dd0bb5273c37d07b1d5e3b5238f0
steinuil <steenuil.owl@gmail.com>
2018-12-04 13:49:52 +0100

    memoized project euler 0015

--- project-euler/0015.fs
+++ project-euler/0015.fs
@@ -1,21 +1,37 @@
-type Lattice =
-  | End
-  | Branch of right : Lattice * down : Lattice
+open System.Collections.Generic
 
-let countPaths side =
-  let paths = ref 0UL
+// type Lattice =
+//   | End
+//   | Branch of right : Lattice * down : Lattice
+
+let countPaths width height : uint64 =
+  let memo = new Dictionary<(int * int), uint64>()
   let rec count x y =
-    if x >= side || y >= side then
-      paths := !paths + 1UL
+    if x >= width || y >= height then
+      1UL
     else
-      count (x + 1) y;
-      count x (y + 1)
-  count 0 0;
-  !paths
+      match memo.TryGetValue ((x, y)) with
+      | true, c -> c
+      | false, _ ->
+        let right = count (x + 1) y
+        let down = count x (y + 1)
+        let both = right + down
+        memo.Add ((x, y), both)
+        both
+  count 0 0
 
 [<EntryPoint>]
 let main args =
-  int args.[0]
-  |> countPaths
+  let (width, height) =
+    match args with
+    | [| width; height |] ->
+      (int width, int height)
+    | [| width |] ->
+      let width = int width
+      (width, width)
+    | _ ->
+      printfn "Usage: 0015 <width> [<height>]"
+      exit 1
+  countPaths width height
   |> printfn "%d";
   0
