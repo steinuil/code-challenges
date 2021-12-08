8c943a47b03c7be71fb1a310487b68b8942c00c6
steinuil <steenuil.owl@gmail.com>
2021-12-08 19:02:39 +0100

    comments and small optimizations

--- advent-of-code-2021/day_08.erl
+++ advent-of-code-2021/day_08.erl
@@ -1,5 +1,5 @@
 -module(day_08).
--export([main/1, digits/0]).
+-export([main/1]).
 -mode(compile).
 
 
@@ -25,43 +25,50 @@ part1(Input) ->
 	end, Input).
 
 
-digits() -> [
-	2#1110111,
-	2#0010010,
-	2#1011101,
-	2#1011011,
-	2#0111010,
-	2#1101011,
-	2#1101111,
-	2#1010010,
-	2#1111111,
-	2#1111011
-].
-
-
 find_digit(String, Positions) ->
 	N = lists:foldl(fun (Pos, Acc) ->
-		I = ekk:list_find_index(Positions, Pos),
+		I = maps:get(Pos, Positions),
 		(1 bsl (6 - I)) bor Acc
 	end, 0, String),
-	ekk:list_find_index(digits(), N).
+	maps:get(N, #{
+		2#1110111 => 0,
+		2#0010010 => 1,
+		2#1011101 => 2,
+		2#1011011 => 3,
+		2#0111010 => 4,
+		2#1101011 => 5,
+		2#1101111 => 6,
+		2#1010010 => 7,
+		2#1111111 => 8,
+		2#1111011 => 9
+	}, false).
 
 
 solve_positions(P) ->
-	[[N1A, N1B] = N1, N7, N4, NX1, NX2, NX3, NX4, NX5, NX6, N8] =
+	% Segments 'c' and 'f' are the segments of 1
+	[[N1A, N1B] = N1, N7, N4, U5_1, U5_2, U5_3, U6_1, U6_2, U6_3, N8] =
 		lists:sort(fun (A, B) -> (length(A) - length(B)) < 0 end, P),
+	% Segment 'a' is the difference between the segments of 7 and 1
 	[P0] = N7 -- N1,
+	% Segments 'b' and 'd' are the difference between the the segments of 4 and 1
 	[N4A, N4B] = N4 -- N1,
-	Unknown = [NX1, NX2, NX3, NX4, NX5, NX6],
+	Unknown = [U5_1, U5_2, U5_3, U6_1, U6_2, U6_3],
+	% Try all permutations of 'b' 'c' 'd' and 'f'
 	{value, Positions} = ekk:find_map(fun ({P2, P5}) ->
 		ekk:find_map(fun ({P1, P3}) ->
-			Known = lists:sort([P0, P1, P2, P3, P5]),
-			case lists:search(fun (N) -> (Known -- N) =:= [] end, Unknown) of
+			Known = [P0, P1, P2, P3, P5],
+			% 9 is the only one of the numbers with 6 segments that contains
+			% all the previously found segments
+			case lists:search(fun (N) -> (Known -- N) =:= [] end, [U6_1, U6_2, U6_3]) of
 				false -> false;
 				{value, N9} ->
+					% 'g' is the only segment of 9 that we don't know yet
 					[P6] = N9 -- Known,
+					% 'e' is the only segment of 8 that is not also in 9
 					[P4] = N8 -- N9,
-					Positions = [P0, P1, P2, P3, P4, P5, P6],
+					Positions = #{P0 => 0, P1 => 1, P2 => 2, P3 => 3, P4 => 4, P5 => 5, P6 => 6},
+					% If this solution is correct then all the remaining numbers
+					% should map to a valid digit on the display.
 					case lists:all(fun (Pattern) -> find_digit(Pattern, Positions) /= false end, Unknown) of
 						true -> {value, Positions};
 						false -> false
