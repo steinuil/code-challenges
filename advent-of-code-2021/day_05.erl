1637832b32edc21fc48f4d07aca240a409a426e2
steinuil <steenuil.owl@gmail.com>
2021-12-09 03:11:26 +0100

    aoc2021: day 5 small optimizations

--- advent-of-code-2021/day_05.erl
+++ advent-of-code-2021/day_05.erl
@@ -7,21 +7,30 @@ parse_line(Line) ->
 	[P1B, P2B] = binary:split(Line, <<" -> ">>, []),
 	[X1B, Y1B] = binary:split(P1B, <<",">>, []),
 	[X2B, Y2B] = binary:split(P2B, <<",">>, []),
-	P1 = {binary_to_integer(X1B), binary_to_integer(Y1B)},
-	P2 = {binary_to_integer(X2B), binary_to_integer(Y2B)},
-	{P1, P2}.
+	{binary_to_integer(X1B), binary_to_integer(Y1B),
+	 binary_to_integer(X2B), binary_to_integer(Y2B)}.
 
 
 encode_coord(X, Y) ->
 	(X bsl 10) bor Y.
 
 
-set_point(X, Y, Map) ->
-	maps:update_with(encode_coord(X, Y), fun (N) -> N + 1 end, 1, Map).
+set_point(X, Y, {One, More} = Map) ->
+	Key = encode_coord(X, Y),
+	case sets:is_element(Key, More) of
+		true -> Map;
+		false ->
+			case sets:is_element(Key, One) of
+				true ->
+					{One, sets:add_element(Key, More)};
+				false ->
+					{sets:add_element(Key, One), More}
+			end
+	end.
 
 
 intersections2(Lines) ->
-	lists:foldl(fun ({{X1, Y1}, {X2, Y2}}, Map) ->
+	lists:foldl(fun ({X1, Y1, X2, Y2}, Map) ->
 		case {X2 - X1, Y2 - Y1} of
 			{0, 0} ->
 				set_point(X1, Y1, Map);
@@ -43,20 +52,15 @@ intersections2(Lines) ->
 			{N, N2} when (N < 0) and (N2 > 0) ->
 				lists:foldl(fun (X, M) -> set_point(X1 - X, Y1 + X, M) end, Map, lists:seq(0, -N))
 		end
-	end, #{}, Lines).
+	end, {sets:new([{version,2}]), sets:new([{version,2}])}, Lines).
 
 
-count_intersections(Map) ->
-	maps:fold(fun (_, V, C) ->
-		if
-			V > 1 -> C + 1;
-			true -> C
-		end
-	end, 0, Map).
+count_intersections({_, More}) ->
+	sets:size(More).
 
 
 intersections1(L1) ->
-	Lines = lists:filter(fun ({{X1, Y1}, {X2, Y2}}) -> (X1 == X2) or (Y1 == Y2) end, L1),
+	Lines = lists:filter(fun ({X1, Y1, X2, Y2}) -> (X1 == X2) or (Y1 == Y2) end, L1),
 	intersections2(Lines).
 
 
