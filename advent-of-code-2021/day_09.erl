951183192d1355d8e331dee77d9c960b49f8b86c
steinuil <steenuil.owl@gmail.com>
2021-12-09 19:04:27 +0100

    aoc2021: refactored day 9 part 1

--- advent-of-code-2021/day_09.erl
+++ advent-of-code-2021/day_09.erl
@@ -7,87 +7,36 @@ read_line(Line) ->
 	array:fix(array:from_list(binary_to_list(Line))).
 
 
+get_xy(A, X, Y) ->
+	try
+		array:get(X, array:get(Y, A))
+	catch _:_ ->
+		10
+	end.
+
+
 is_low_point(Point, Around) ->
 	case lists:all(fun (P) -> P > Point end, Around) of
-		true ->
-		  Point - $0 + 1;
+		true -> Point - $0 + 1;
 		false -> 0
 	end.
 
 
-get_xy(A, X, Y) ->
-	array:get(X, array:get(Y, A)).
-
-
-low_points(_, x, _, _, _, Acc) ->
-	Acc;
-low_points(Input, X, Y, MaxX, MaxY, Acc)  ->
-	%io:format("~w,~w = ~w\n", [X, Y, get_xy(Input, X, Y) - $0]),
-
-	{P, NextX, NextY} = if
-		(X == 0) and (Y == 0) ->
-			Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X, Y + 1)
-			]),
-			{Point, X + 1, Y};
-		(X == 0) and (Y == MaxY) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, X + 1, Y};
-		(X == MaxX) and (Y == 0) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y + 1)
-			]),
-			{Point, 0, Y + 1};
-		(X == MaxX) and (Y == MaxY) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, x, Y};
-		(X == 0) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X, Y + 1),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, X + 1, Y};
-		(X == MaxX) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y + 1),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, 0, Y + 1};
-		(Y == 0) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y + 1)
-			]),
-			{Point, X + 1, Y};
-		(Y == MaxY) ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, X + 1, Y};
-		true ->
-		  Point = is_low_point(get_xy(Input, X, Y), [
-				get_xy(Input, X + 1, Y),
-				get_xy(Input, X - 1, Y),
-				get_xy(Input, X, Y + 1),
-				get_xy(Input, X, Y - 1)
-			]),
-			{Point, X + 1, Y}
-	end,
-
-	low_points(Input, NextX, NextY, MaxX, MaxY, Acc + P).
+low_points(Input, X, Y, MaxX, MaxY, Acc)	->
+	P = is_low_point(get_xy(Input, X, Y), [
+		get_xy(Input, X + 1, Y),
+		get_xy(Input, X - 1, Y),
+		get_xy(Input, X, Y + 1),
+		get_xy(Input, X, Y - 1)
+	]),
+	case {X, Y} of
+		{MaxX, MaxY} ->
+			Acc + P;
+		{MaxX, _} ->
+			low_points(Input, 0, Y + 1, MaxX, MaxY, Acc + P);
+		_ ->
+			low_points(Input, X + 1, Y, MaxX, MaxY, Acc + P)
+	end.
 
 
 main([File]) ->
